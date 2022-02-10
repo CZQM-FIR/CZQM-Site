9e5e5ced3a8707a8ad7a4015da0cab7db87375e1
JagardStrong <Jagard_strong@hotmail.com>
2022-02-10 09:16:55 -0330

    Altered Home page text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
 <main>
     <h1>Welcome to the Moncton FIR.</h1>
-    <p>The Moncton/Gander FIR provides professional, courteous and realistic Air Traffic Control services for the Virtual Moncton & Gander Flight Information Region on the VATSIM Network.</p>
+    <p>The Moncton/Gander FIR provides professional, courteous and realistic Air Traffic Control services for the Virtual Moncton & Gander Flight Information Region on the VATSIM<sub>&#x000A9</sub>Network.</p>
     <p>Our airspace includes all of New Brunswick, Nova Scotia, Prince Edward Island, Newfoundland and Labrador, as well as Eastern Québec.</p>
     <p>All virtual pilots are invited to fly the friendly skies of Moncton/Gander FIR!</p>
 </main>
@@ -17,4 +17,15 @@
 main {
     font-size: 18px;
 }
+
+H1 {
+    text-transform: uppercase;
+}
+
+p { 
+    margin: 3px;
+}
+
+
+
 </style>
