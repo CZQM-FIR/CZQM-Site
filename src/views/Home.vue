bf9557bf557e2d6b72dd1cea88438ba340eb2b13
GoldenXLence <jack@koskie.ca>
2022-02-10 16:34:36 -0500

    chore(formatting): change css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
-<main>
-    <h1>Welcome to the Moncton FIR.</h1>
-    <p>The Moncton/Gander FIR provides professional, courteous and realistic Air Traffic Control services for the Virtual Moncton & Gander Flight Information Region on the VATSIM Network<sub>&#x000A9</sub>.</p>
+<main class="home-page">
+    <h1 class="main-header">Welcome to the Moncton FIR.</h1>
+    <p>The Moncton/Gander FIR provides professional, courteous and realistic Air Traffic Control services for the Virtual Moncton & Gander Flight Information Region on the VATSIM Network.</p>
     <p>Our airspace includes all of New Brunswick, Nova Scotia, Prince Edward Island, Newfoundland and Labrador, as well as Eastern Québec.</p>
     <p>All virtual pilots are invited to fly the friendly skies of Moncton/Gander FIR!</p>
     <img src="src\assets\images\CZQM.png" alt="CZQM Logo" class="CZQM_Main_logo">
@@ -15,16 +15,12 @@
 </script>
 
 <style scoped>
-main {
-    font-size: 18px;
+.home-page {
+    font-size: 1.5rem;
 }
 
-H1 {
+.main-header {
     text-transform: uppercase;
-}
-
-p { 
-    margin: 3px;
 }
 
 .CZQM_Main_logo {
