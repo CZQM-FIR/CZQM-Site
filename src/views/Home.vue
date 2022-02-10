a2dbf864107742ba89f16baeecf791305c8641a1
JagardStrong <Jagard_strong@hotmail.com>
2022-02-10 09:21:13 -0330

    Added CZQM logo to <main> ele

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,7 @@
     <p>The Moncton/Gander FIR provides professional, courteous and realistic Air Traffic Control services for the Virtual Moncton & Gander Flight Information Region on the VATSIM Network<sub>&#x000A9</sub>.</p>
     <p>Our airspace includes all of New Brunswick, Nova Scotia, Prince Edward Island, Newfoundland and Labrador, as well as Eastern Québec.</p>
     <p>All virtual pilots are invited to fly the friendly skies of Moncton/Gander FIR!</p>
+    <img class="CZQM Logo" src="C:\Users\Jagar\Desktop\VATCAN\CZQM-Site\src\assets\images\CZQM.png" alt="CZQM Logo" >
 </main>
 </template>
 
