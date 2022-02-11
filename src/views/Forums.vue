e30f7bd86dc03d53e896e53432a12cdb856b494c
Jack Koskie <65452167+GoldenXLence@users.noreply.github.com>
2022-02-11 08:33:38 -0500

    feat: add vatcan forum redirec (untested)

---
+++ src/views/Forums.vue
@@ -0,0 +1,18 @@
+<template>
+<main class="redirect">
+    <h1>Redirecting</h1>
+</main>
+
+<meta http-equiv="Refresh" content="0; url='https://forums.vatcan.ca/'" />
+</template>
+
+<script>
+    export default {
+    name: "Forum Redirect",
+}
+</script>
+
+<style scoped>
+
+
+</style>
