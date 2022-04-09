ceb2f1381e92aec743aaecd0d64dfbae424951e8
GoldenXLence <jack@koskie.ca>
2022-04-09 11:01:52 -0400

    feat: update role name based on id

--- routes/api/user.js
+++ routes/api/user.js
@@ -7,6 +7,56 @@ const router = Router();
 router.get('/:jwt', async (req, res) => {
     let user = await User.findOne({ jwt: req.params.jwt });
 
+    switch (user.role.id) {
+        case 0:
+            if (user.role.name !== 'Guest') {
+                user.role.name = 'Guest';
+                user.save();
+            }
+            break;
+
+        case 1:
+            if (user.role.name !== 'Visiting Controller') {
+                user.role.name = 'Visiting Controller';
+                user.save();
+            }
+            break;
+
+        case 2:
+            if (user.role.name !== 'Home Controller') {
+                user.role.name = 'Home Controller';
+                user.save();
+            }
+            break;
+
+        case 3:
+            if (user.role.name !== 'Mentor') {
+                user.role.name = 'Mentor';
+                user.save();
+            }
+            break;
+
+        case 4:
+            if (user.role.name !== 'Instructor') {
+                user.role.name = 'Instructor';
+                user.save();
+            }
+            break;
+
+        case 5:
+            if (user.role.name !== 'FIR Staff') {
+                user.role.name = 'FIR Staff';
+                user.save();
+            }
+            break;
+
+        default:
+            user.role.id = 0;
+            user.role.name = 'Guest';
+            user.save();
+            break;
+    }
+
     if (!user) {
         return res.status(404).send('User not found');
     }
