809e66367773874e9d7792ceca5e1548f06a6507
GoldenXLence <jack@koskie.ca>
2022-06-22 17:01:49 -0400

    chore: add user management button

--- packages/client/src/components/StaffPortal/Navigation.vue
+++ packages/client/src/components/StaffPortal/Navigation.vue
@@ -1,41 +1,42 @@
 <template>
     <div>
-        <h1>{{greeting}} {{user.personal.name_first}}!</h1>
+        <h1>{{ greeting }} {{ user.personal.name_first }}!</h1>
         <div class="cta-row">
-            <router-link :to="{ query: { page: 'events' }}" class="cta" >Events</router-link>
-            <router-link :to="{ query: { page: 'news' }}" class="cta" >News</router-link>
+            <router-link :to="{ query: { page: 'events' } }" class="cta">Events</router-link>
+            <router-link :to="{ query: { page: 'news' } }" class="cta">News</router-link>
+            <router-link :to="{ query: { page: 'users' } }" class="cta">User Management</router-link>
         </div>
     </div>
 </template>
 
 <script>
-    import getUser from "../../scripts/getUser";
-    import { ref } from "vue";
-    
-    export default {
-        setup: async () => {
+import getUser from "../../scripts/getUser";
+import { ref } from "vue";
 
-            // Get user info
-            let user = ref(await getUser());
+export default {
+    setup: async () => {
 
-            // Get current greeting
-            let hour = new Date().getHours();
-            let greeting = ref(`Good ${hour < 12 ? "Morning" : hour < 18 ? "Afternoon" : "Evening"}`);
+        // Get user info
+        let user = ref(await getUser());
 
-            return {
-                greeting,
-                user
-            };
-        }
+        // Get current greeting
+        let hour = new Date().getHours();
+        let greeting = ref(`Good ${hour < 12 ? "Morning" : hour < 18 ? "Afternoon" : "Evening"}`);
+
+        return {
+            greeting,
+            user
+        };
     }
+}
 </script>
 
 <style lang="scss" scoped>
-    h1 {
-        align-self: center;
-    }
+h1 {
+    align-self: center;
+}
 
-    .cta-row {
-        justify-content: center;
-    }
+.cta-row {
+    justify-content: center;
+}
 </style>
