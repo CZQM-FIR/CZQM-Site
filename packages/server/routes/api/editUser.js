9978e1e5d808d97fad6e1dfba31b91286176fc1f
GoldenXLence <jack@koskie.ca>
2022-06-29 08:26:48 -0400

    chore: remove console log

--- packages/server/routes/api/editUser.js
+++ packages/server/routes/api/editUser.js
@@ -4,8 +4,6 @@ const User = require('../../models/User')
 const router = Router()
 
 router.get('/', async (req, res) => {
-    console.info(req.query.user)
-
     const userParam = JSON.parse(req.query.user)
     if (!userParam) {
         res.status(400).json({
@@ -14,18 +12,16 @@ router.get('/', async (req, res) => {
     }
 
     try {
-        let user = await User.findOne({ cid: userParam.cid })
+        const user = await User.findOne({ cid: userParam.cid })
         user.personal.name_first = userParam.personal
         user.role = userParam.role
         user.roster = userParam.roster
 
         await user.save()
 
-        console.info('save success')
-
         return res.status(200)
     } catch (error) {
-        res.status(500)
+        return res.status(500)
     }
 })
 
