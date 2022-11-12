8b42ba7d9eb006266f70ba1438c3460cb0a27c25
GoldenXLence <jack@koskie.ca>
2022-11-11 21:13:38 -0500

    chore: change dev port

--- packages/client/vite.config.js
+++ packages/client/vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   plugins: [vue()],
   server: {
+    port: 3000,
     proxy: {
       '/api': {
         target: 'http://localhost:3500',
