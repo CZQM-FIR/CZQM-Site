08276cc3098bf43b2537adfde8375c10e349fc6b
GoldenXLence <65452167+GoldenXLence@users.noreply.github.com>
2022-11-16 21:12:33 -0500

    feat: add metar caching

--- packages/server/routes/api/metar.js
+++ packages/server/routes/api/metar.js
@@ -3,6 +3,9 @@ const { Router } = require("express");
 
 const router = Router();
 
+// Array of objects: {icao: "", metar: "", time: unix timestamp}
+const metarCache = [];
+
 router.get("/:icao", async (req, res) => {
   const { icao } = req.params;
 
@@ -13,7 +16,27 @@ router.get("/:icao", async (req, res) => {
         .json({ message: "ICAO code must be 4 characters long" });
     }
 
-    const metar = await axios.get(`https://metar.vatsim.net/?id=${icao}`);
+    let metar
+
+    const date = new Date(Date.now())
+    const time = date.getTime()
+
+    const setExpiration = (array, element, delay) => {
+      setTimeout(() => {
+        const index = array.indexOf(element)
+      if (index > -1) {
+        array.splice(index, 1)
+      }
+      }, delay)
+    }
+
+    if (metarCache.some((element) => element.icao.toLowerCase() === icao.toLowerCase() && (Number(time) - Number(element.time)) < 30*1000)) {
+      metar = await metarCache.find((element) => element.icao === icao)
+    } else {
+      metar = await axios.get(`https://metar.vatsim.net/?id=${icao}`);
+      const element = metarCache.push({ icao, data: metar.data, time: Number(time) });
+      setExpiration(metarCache, element, 1)
+    }
 
     if (metar.data.length === 0) {
       return res
