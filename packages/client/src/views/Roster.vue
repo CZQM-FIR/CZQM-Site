ca81a75e25039445fe3e46283a62ce770baee78b
Jagardstrong <99367993+Jagardstrong@users.noreply.github.com>
2022-11-30 19:31:59 -0330

    testing

--- packages/client/src/views/Roster.vue
+++ packages/client/src/views/Roster.vue
@@ -104,7 +104,7 @@ export default {
 
 .roster thead tr {
   background-color: var(--nav-colour);
-  color: rgb(165, 12, 12);
+  color: rgb(124, 121, 121);
   text-align: left;
   font-weight: bold;
 }
