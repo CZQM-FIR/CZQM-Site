c37fb31cd818df3d5465dd8f387342e89f19f50b
GoldenXLence <65452167+GoldenXLence@users.noreply.github.com>
2022-12-03 14:11:34 -0500

    Revert "chore: change default port"
    This reverts commit 00470f67ef8dae284dc2bc95b8a0630e0d728550.

--- packages/server/config.js
+++ packages/server/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
     mongoUri: process.env.MONGO_URI,
-    PORT: process.env.PORT || 80,
+    PORT: process.env.PORT || 3500,
     transporterConfig: {
         host: 'smtppro.zoho.com',
         port: 465,
