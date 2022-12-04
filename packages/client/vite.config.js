868dd8de9977e4daa7bbc0c65a6bef9e2a9205ef
GoldenXLence <65452167+GoldenXLence@users.noreply.github.com>
2022-12-03 19:05:44 -0500

    feat: replace proxies on build

--- packages/client/vite.config.js
+++ packages/client/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import replace from '@rollup/plugin-replace';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,4 +18,17 @@ export default defineConfig({
       },
     },
   },
+
+  build: {
+    rollupOptions: {
+      // https://rollupjs.org/guide/en/#big-list-of-options
+      plugins: [
+        replace({
+          delimiters: ['(?<!\\.)(?<!3500)\\/', ''],
+            'api': 'http://dev.czqm.ca:3500/api',
+            'files': 'http://dev.czqm.ca:3500/files',
+        })
+      ]
+    }
+  }
 });
