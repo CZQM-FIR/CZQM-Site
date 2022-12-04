07a6c06400c2229ab9c23448d1fbb8db51336d49
GoldenXLence <65452167+GoldenXLence@users.noreply.github.com>
2022-12-04 14:31:54 -0500

    fix: remove airspace map width constraint

--- packages/client/src/components/ControllerResources/AirspaceMap.vue
+++ packages/client/src/components/ControllerResources/AirspaceMap.vue
@@ -3,7 +3,6 @@
         <h2>CZQM/CZQX Airspace Map</h2>
         <iframe 
             src="https://www.google.com/maps/d/embed?mid=12Pe1bg7JBSaDsB0ZHuCmggFHs6TCVPHr&ehbc=2E312F" 
-            width="640" 
             height="480"
             >
         </iframe>
