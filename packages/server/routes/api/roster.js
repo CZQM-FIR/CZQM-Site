a9b2cf82a4c53a65627ed691b50c84f6b21ef11e
GoldenXLence <65452167+GoldenXLence@users.noreply.github.com>
2022-12-09 18:35:58 -0500

    feat: sort roster by first role then last name

--- packages/server/routes/api/roster.js
+++ packages/server/routes/api/roster.js
@@ -21,14 +21,30 @@ router.get('/', async (req, res) => {
     })
 
     controllerList.sort((a, b) => { 
-        const nameA = a.name
-        const nameB = b.name
+        const roleOrder = [
+          'Guest',
+          'Visitor',
+          'Home Controller',
+          'Mentor',
+          'Instructor',
+          'Events',
+          'Sector Engineer',
+          'Chief Instructor',
+          'Deputy Chief',
+          'Webmaster',
+          'FIR Chief'
+        ]
 
-        if (nameA < nameB) {
+        const nameA = a.name.split(' ')[1]
+        const nameB = b.name.split(' ')[1]
+        const roleA = roleOrder.indexOf(a.role)
+        const roleB = roleOrder.indexOf(b.role)
+
+        if ((nameA < nameB && roleA > roleB) || (nameA === nameB && roleA > roleB) || (nameA < nameB && roleA === roleB)) {
             return -1
         }
 
-        if (nameA > nameB) {
+        if ((nameA > nameB && roleA < roleB) || (nameA === nameB && roleA < roleB) || (nameA > nameB && roleA === roleB)) {
             return 1
         }
 
