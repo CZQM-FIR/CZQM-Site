60b7d57a93d7f410e7ececda5c3df7f27a2935e9
GoldenXLence <65452167+GoldenXLence@users.noreply.github.com>
2022-12-18 10:21:16 -0500

    feat: add new role system to roster

--- packages/client/src/views/Roster.vue
+++ packages/client/src/views/Roster.vue
@@ -43,7 +43,7 @@
                     : 'Inactive'
             }}
           </td>
-          <td>{{ controller.role.name }}</td>
+          <td>{{ controller.role }}</td>
         </tr>
       </tbody>
       <tbody v-else>
