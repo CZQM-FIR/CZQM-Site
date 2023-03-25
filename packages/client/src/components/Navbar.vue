c33ab1589de0e7b78420bf523a210db764290d52
GoldenXLence <65452167+GoldenXLence@users.noreply.github.com>
2023-03-25 17:13:28 -0400

    fix: re-add deleted navbar styles

--- packages/client/src/components/Navbar.vue
+++ packages/client/src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="navbar-wrapper container" :class="{ active: showMobileMenu }">
+  <div class="navbar-wrapper container" :class="{ active: showMobileMenu }" >
     <nav class="navbar">
       <ul class="navitems">
         <!-- CZQM Logo Element -->
@@ -11,11 +11,7 @@
               class="logo"
             />
           </router-link>
-          <div
-            class="hamburger"
-            :class="{ active: showMobileMenu }"
-            @click="showMobileMenu = !showMobileMenu"
-          >
+          <div class="hamburger" :class="{ active: showMobileMenu }" @click="showMobileMenu = !showMobileMenu">
             <span class="bar"></span>
             <span class="bar"></span>
             <span class="bar"> </span>
@@ -38,62 +34,34 @@
 
         <li class="navitem">
           <div class="dropdown">
-            <span
-              class="navitem-text"
-              v-on:click="showDropdownControllers = !showDropdownControllers"
-              >Controllers</span
-            >
-            <ul
-              :class="{
-                'dropdown-content': true,
-                'dropdown-hover': showDropdownControllers && showMobileMenu,
-              }"
-            >
+            <span class="navitem-text" v-on:click="showDropdownControllers = !showDropdownControllers">Controllers</span>
+            <ul :class="{'dropdown-content': true, 'dropdown-hover': showDropdownControllers && showMobileMenu}">
               <li class="dropdown-text">
                 <div class="dropdown-icon">
                   <i class="fa-solid fa-clipboard-user"></i>
                 </div>
-                <router-link to="/staff" @click="showMobileMenu = false">
-                  Staff
-                </router-link>
+                <router-link to="/staff" @click="showMobileMenu = false"> Staff </router-link>
               </li>
               <li class="dropdown-text">
                 <div class="dropdown-icon">
                   <i class="fa-solid fa-rectangle-list"></i>
                 </div>
-                <router-link to="/roster" @click="showMobileMenu = false">
-                  Roster
-                </router-link>
+                <router-link to="/roster" @click="showMobileMenu = false"> Roster </router-link>
               </li>
-              <div
-                v-if="
-                  user.loggedIn == false ||
-                  user.flags.some(
-                    (flag) => flag === 'guest' || flag === 'staff'
-                  )
-                "
-              >
+              <div v-if="user.loggedIn == false || user.flags.some((flag) => flag === 'guest' || flag === 'staff')">
                 <li class="dropdown-text">
                   <div class="dropdown-icon">
                     <i class="fa-solid fa-user-plus"></i>
                   </div>
-                  <router-link to="/join" @click="showMobileMenu = false">
-                    Join Us
-                  </router-link>
+                  <router-link to="/join" @click="showMobileMenu = false"> Join Us </router-link>
                 </li>
               </div>
-              <div
-                v-if="
-                  user.loggedIn && !user.flags.some((flag) => flag === 'guest')
-                "
-              >
+              <div v-if="user.loggedIn && !user.flags.some((flag) => flag === 'guest')">
                 <li class="dropdown-text">
                   <div class="dropdown-icon">
                     <i class="fa-solid fa-file-lines"></i>
                   </div>
-                  <router-link to="/resources" @click="showMobileMenu = false"
-                    >Resources</router-link
-                  >
+                  <router-link to="/resources" @click="showMobileMenu = false">Resources</router-link>
                 </li>
               </div>
             </ul>
@@ -112,12 +80,11 @@
           </a>
         </li>
 
+        <!-- Login MUST BE LAST!!! -->
+
         <li class="navitem login">
           <div v-if="user.loggedIn">
-            <div
-              class="dropdown"
-              v-on:click="showDropdownLogin = !showDropdownLogin"
-            >
+            <div class="dropdown" v-on:click="showDropdownLogin = !showDropdownLogin">
               <span class="navitem-text"
                 >{{ user.personal.name_full }} (<span class="navitem-number">{{
                   user.cid
@@ -125,43 +92,27 @@
                 >)</span
               >
 
-              <ul
-                :class="{
-                  'dropdown-content': true,
-                  'dropdown-hover': showDropdownLogin && showMobileMenu,
-                }"
-              >
+              <ul :class="{'dropdown-content': true, 'dropdown-hover': showDropdownLogin && showMobileMenu}">
                 <li class="dropdown-text">
                   <div class="dropdown-icon">
                     <i class="fa-solid fa-cloud"></i>
                   </div>
-                  <router-link to="/portal" @click="showMobileMenu = false"
-                    >Portal</router-link
-                  >
+                  <router-link to="/portal" @click="showMobileMenu = false">Portal</router-link>
                 </li>
                 <li
                   class="dropdown-text"
-                  v-if="
-                    user.loggedIn &&
-                    user.flags.some((flag) => flag.startsWith('staff'))
-                  "
+                  v-if="user.loggedIn && user.flags.some((flag) => flag.startsWith('staff'))"
                 >
                   <div class="dropdown-icon">
                     <i class="fa-solid fa-bars-progress"></i>
                   </div>
-                  <router-link
-                    to="/staff-portal"
-                    @click="showMobileMenu = false"
-                    >Staff Portal</router-link
-                  >
+                  <router-link to="/staff-portal" @click="showMobileMenu = false">Staff Portal</router-link>
                 </li>
                 <li class="dropdown-text">
                   <div class="dropdown-icon">
                     <i class="fa-solid fa-arrow-right-from-bracket"></i>
                   </div>
-                  <router-link to="/logout" @click="showMobileMenu = false"
-                    >Logout</router-link
-                  >
+                  <router-link to="/logout" @click="showMobileMenu = false">Logout</router-link>
                 </li>
               </ul>
             </div>
@@ -172,7 +123,9 @@
             </a>
           </div>
         </li>
+        
       </ul>
+
     </nav>
   </div>
 </template>
@@ -193,7 +146,7 @@ export default {
 
     let user = ref(userData);
 
-    console.log(user.value);
+    console.log(user.value)
 
     return {
       user,
@@ -203,10 +156,213 @@ export default {
     return {
       showMobileMenu: false,
       showDropdownControllers: false,
-      showDropdownLogin: false,
-    };
-  },
+      showDropdownLogin: false
+    }
+  }
 };
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+@import '../assets/styles/global.scss';
+
+.navbar-wrapper {
+  width: 100vw;
+  height: var(--nav-size);
+  top: 0;
+  left: 0;
+  position: fixed;
+  background: var(--nav-colour);
+  z-index: 100;
+}
+
+.navbar {
+  margin: 0;
+  padding: 0;
+  width: var(--width-ideal);
+  max-width: var(--width-max);
+  height: 100%;
+}
+
+.navitems {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  align-items: center;
+  height: var(--nav-size);
+  gap: 1rem;
+}
+
+.navitem {
+  width: auto;
+  height: var(--nav-size);
+  display: flex;
+  align-items: center;
+}
+
+.navitem a {
+  text-decoration: none;
+}
+
+.login {
+  margin-left: auto;
+  margin-right: 1rem;
+}
+
+.navitem svg {
+  width: 3rem;
+  max-height: calc(var(--nav-size) * 0.9);
+  color: white;
+}
+
+.navitem-text {
+  color: #c9c1c1;
+  font-size: 1rem;
+  margin-left: 1rem;
+  font-family: 'Raleway', sans-serif;
+  transition: 0.1s ease;
+  text-transform: uppercase;
+}
+
+.navitem-number {
+  font-family: 'Arimo', sans-serif;
+}
+
+.navitem-text:hover {
+  color: white;
+}
+
+.logo {
+  max-height: calc(var(--nav-size) * 0.9);
+}
+
+/* Dropdowns */
+
+.dropdown {
+  position: relative;
+  display: inline-block;
+  transition: 1s ease;
+}
+
+.dropdown-content {
+  display: block;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 7rem;
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+  padding: 12px 16px;
+  padding-left: 0;
+  margin-left: 0;
+  list-style-type: none;
+  /* opacity: 0; */
+  visibility: hidden;
+}
+
+.dropdown-icon svg {
+  color: var(--nav-colour);
+  height: 1rem;
+  margin: 0;
+  padding: 0;
+}
+
+.dropdown-text {
+  font-family: 'Raleway', sans-serif;
+  display: flex;
+  text-transform: capitalize;
+}
+
+.dropdown:hover .dropdown-content,
+.dropdown-hover {
+  /* opacity: 1; */
+  visibility: visible;
+  transition: 1s ease;
+}
+
+.dropdown a {
+  color: black;
+}
+
+.hamburger {
+  display: none;
+  cursor: pointer;
+  transition: all ease-in-out 0.25s;
+
+}
+
+.bar {
+  display: block;
+  width: 25px;
+  height: 3px;
+  margin: 5px auto;
+  -webkit-transition: all 0.3 ease-in-out;
+  transition: all 0.3 ease-in-out;
+  background-color: white;
+}
+
+.czqm-logo {
+  display: block;
+}
+
+@media only screen and (max-width: 950px) {
+  .navitems {
+    flex-direction: column;
+    // opacity: 0;
+  }
+
+  .navitem-text {
+    font-size: 1.25rem;
+  }
+
+  .login {
+    margin: unset;
+    padding-left: 0;
+  }
+  
+  .hamburger {
+    display: block !important;
+    margin-left: auto;
+  }
+
+  .czqm-logo {
+    opacity: unset !important;
+    display: flex !important;
+    flex-direction: row;
+    align-items: center;
+    width: 100%;
+  }
+
+  .logo {
+    margin-right: auto;
+  }
+
+  .hamburger {
+    margin-left: auto;
+    padding-left: auto;
+  }
+
+  .hamburger.active .bar:nth-child(2){
+    opacity: 0;
+  }
+
+  .hamburger.active .bar:nth-child(1){
+    transform: translateY(8px) rotate(45deg);
+  }
+
+  .hamburger.active .bar:nth-child(3){
+    transform: translateY(-8px) rotate(-45deg);
+  }
+
+  .navbar-wrapper.active {
+    background-color: var(--nav-colour);
+    height: 100%;
+  }
+
+  .navbar-wrapper > .navbar > .navitems > :not(.czqm-logo) {
+    display: none;
+  }
+
+  .navbar-wrapper.active > .navbar > .navitems > :not(.czqm-logo) {
+    display: block;
+  }
+}
+</style>
